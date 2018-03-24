bc5f678a604e25f53eb97852c47a9582246ce8a4
DKoeltzow <d.koeltzow@web.de>
2018-03-24 13:12:31 +0100

    Initial commit

---
+++ Knight.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    class Knight
+    {
+        private void Start()
+        {
+            Console.WriteLine("I am the Knight... this city deserves but not need or something like that");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Reflection
+{
+    class Program
+    {
+        private static List<object> objects = new List<object>();
+
+        static void Main(string[] args)
+        {
+            ShowClasses();
+
+            objects.Add(new Knight());
+            objects.Add(new Archer());
+            
+
+            InvokeUnknownMethodByName();
+            
+            Console.Read();
+        }
+
+        private static void ShowClasses()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Type[] types = assembly.GetTypes();
+
+            foreach (var type in types)
+            {
+                if (type.IsAbstract)
+                {
+                    Console.WriteLine(type.Name);
+                }
+            }
+        }
+
+        private static void InvokeUnknownMethodByName()
+        {
+            foreach (var item in objects)
+            {
+                Type type = item.GetType();
+                MethodInfo mInfo = type.GetMethod("Start", BindingFlags.Instance | BindingFlags.NonPublic);
+                if (mInfo != null)
+                {
+                    mInfo.Invoke(item, null);
+                }
+            }
+        }
+    }
+}
